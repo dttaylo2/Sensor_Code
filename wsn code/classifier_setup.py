f1dc0b0f7eed63ac8a9b00bfaffb9ba2b36bdf0d
Adam McNeilly <amcneilly331@gmail.com>
2016-04-12 09:59:05 -0400

    Created script to build classifier model from trained weights

---
+++ wsn code/classifier_setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+import pickle
+import pandas as pd
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout
+from keras.layers.normalization import BatchNormalization
+from keras.optimizers import SGD
+from keras.utils.np_utils import to_categorical
+
+model_weights = pickle.load(open('model_weights.pkl', 'rb'))
+training_data = pd.read_csv('../ML/training_data.csv')
+
+X = training_data.iloc[:, :-1].values
+y = training_data.iloc[:, -1].values
+y_ohe = to_categorical(y)
+
+model = Sequential()
+model.add(BatchNormalization(input_shape=(X.shape[1],), weights = model_weights[0]))
+model.add(Dense(output_dim=500, activation='tanh', weights =  model_weights[1]))
+model.add(Dense(output_dim=350, activation='tanh', weights = model_weights[2]))
+model.add(Dense(output_dim=200, activation='tanh', weights = model_weights[3]))
+model.add(Dense(output_dim=50, activation='tanh', weights = model_weights[4]))
+model.add(Dropout(0.2))
+model.add(Dense(output_dim=y_ohe.shape[1], activation='softmax', weights = model_weights[6]))
+
+sgd = SGD(lr=0.001, decay=1e-7, momentum=0.9)
+model.compile(loss='categorical_crossentropy', optimizer=sgd)
